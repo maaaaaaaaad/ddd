29e7ed73ca7a474a672620b2129af88063f348e1
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-05-28 10:13:51 +0000

    e

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
-import tkinter as tk
-import re
-import tkinter.messagebox
-import webbrowser
-
-root = tk.Tk()
-
-root.title("My GUI Program")
-
-root.geometry("400x400")
-
-label = tk.Label(root, text="Enter domain:")
-label.grid(row=0, column=0)
-
-entry = tk.Entry(root)
-entry.grid(row=0, column=1)
+from collections import defaultdict, deque
+import heapq
 
 
-def submit():
-    domain = entry.get()
-    if re.match("^[a-z]+://[^\s]+", domain):
-        webbrowser.open(domain)
-    else:
-        tk.messagebox.showerror("Error", "Invalid domain address.")
+def solution(edges, target):
 
+  graph = defaultdict(list)
+  for u, v in edges:
+    graph[u].append(v)
 
-submit = tk.Button(root, text="Submit", command=submit)
-submit.grid(row=1, column=1)
+  child = defaultdict(list)
+  for u in graph:
+    child[u] = sorted(graph[u])
 
-root.mainloop()
+  curr_child = {node: 0 for node in child}
+
+  result = []
+
+  target_leaves = [(target[i], i + 1) for i in range(len(target))
+                   if target[i] != 0]
+
+  heapq.heapify(target_leaves)
+
+  while target_leaves:
+    num, leaf = heapq.heappop(target_leaves)
+    if target[leaf - 1] == 0:
+      continue
+
+    path = []
+    node = 1
+    while node != leaf:
+      path.append(node)
+
+      if not child[node]:
+        break
+      node = child[node][curr_child[node]]
+
+    if node != leaf:
+      return [-1]
+
+    result.append(num)
+    target[leaf - 1] = 0
+
+    for node in path:
+      curr_child[node] = (curr_child[node] + 1) % len(child[node])
+  return result if not any(target) else [-1]
